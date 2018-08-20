872a24bba59de3043acc550f5fcf1142a4e399a3
boncis <wendigotech@gmail.com>
2018-08-20 14:36:16 +0200

    58

--- index.php
+++ index.php
@@ -12,21 +12,19 @@ get_header(); ?>
         <?php if ( $casino->have_posts() ) : ?>
             <div class="row grid-content">
                 <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
-                    <?php if ( in_the_loop() ) : ?>
-                        <figure1 style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
-                            <?php
-                                if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail( 'normal' );
-                                }
-                             ?>
-                            <figcaption>
-                                <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                                <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                                <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                                <?php the_excerpt( ); ?>
-                            </figcaption>
-                        </figure1>
-                    <?php endif; ?>
+                    <figure1 style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                        <?php
+                            if ( has_post_thumbnail() ) {
+                                the_post_thumbnail( 'normal' );
+                            }
+                         ?>
+                        <figcaption>
+                            <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                            <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                            <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                            <?php the_excerpt( ); ?>
+                        </figcaption>
+                    </figure1>
                 <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
                 <?php if ( in_the_loop() ) : ?>
