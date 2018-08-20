0deb7dc58d6e3fbb09be819c168a8442c8092fbb
boncis <wendigotech@gmail.com>
2018-08-21 00:54:18 +0200

    74

--- index.php
+++ index.php
@@ -3,10 +3,9 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper"> 
     <?php if ( have_posts() ) : ?>
-        <div class="grid-content container">
-            <?php $item_number = 0; ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur<?php if( $item_number == 0) echo ' grid-content'; ?>">
+        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="grid-content container">
+                <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
                     <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail( 'normal' );
@@ -19,22 +18,8 @@ get_header(); ?>
                         <?php the_excerpt( ); ?>
                     </figcaption>
                 </figure>
-                <?php $item_number++; ?>
-            <?php endwhile; ?>
-            <figure style="grid-area:5 / 2 / 8 / 4;" class="imghvr-blur">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail( 'normal' );
-                    }
-                 ?>
-                <figcaption>
-                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                    <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                    <?php the_excerpt( ); ?>
-                </figcaption>
-            </figure>
-        </div>
+            </div>
+        <?php endwhile; ?>
     <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
     <?php endif; ?> 
