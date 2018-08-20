27fe5cd8cd18372e2ec748b26a47c3fad9695c79
boncis <wendigotech@gmail.com>
2018-08-21 00:49:20 +0200

    72

--- index.php
+++ index.php
@@ -25,6 +25,12 @@ get_header(); ?>
                         the_post_thumbnail( 'normal' );
                     }
                  ?>
+                <figcaption>
+                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                    <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                    <?php the_excerpt( ); ?>
+                </figcaption>
             </figure>
         </div>
     <?php else : ?>
