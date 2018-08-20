ae3dc0c3ab791240baef4853cdcbc777069706b7
boncis <wendigotech@gmail.com>
2018-08-20 14:31:42 +0200

    57

--- index.php
+++ index.php
@@ -3,16 +3,34 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper"> 
     <div class="container">
-        <div class="row grid-content">
-            <?php
-                $casino_args = array(
-                    'category_name' => 'casino'
-                )
-            ?>
-            <?php $casino = new WP_Query( $casino_args ); ?>
-            <?php if ( $casino->have_posts() ) : ?>
+        <?php
+            $casino_args = array(
+                'category_name' => 'casino'
+            )
+        ?>
+        <?php $casino = new WP_Query( $casino_args ); ?>
+        <?php if ( $casino->have_posts() ) : ?>
+            <div class="row grid-content">
                 <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
-                    <figure1 style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                    <?php if ( in_the_loop() ) : ?>
+                        <figure1 style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal' );
+                                }
+                             ?>
+                            <figcaption>
+                                <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                                <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                                <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                                <?php the_excerpt( ); ?>
+                            </figcaption>
+                        </figure1>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+                <?php if ( in_the_loop() ) : ?>
+                    <figure2 style="grid-area:1 / 3 / 3 / 4;" class="imghvr-blur">
                         <?php
                             if ( has_post_thumbnail() ) {
                                 the_post_thumbnail( 'normal' );
@@ -24,28 +42,12 @@ get_header(); ?>
                             <h6 class="text-center display-5"><?php the_title(); ?></h6>
                             <?php the_excerpt( ); ?>
                         </figcaption>
-                    </figure1>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-            <?php if ( in_the_loop() ) : ?>
-                <figure2 style="grid-area:1 / 3 / 3 / 4;" class="imghvr-blur">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                        <?php the_excerpt( ); ?>
-                    </figcaption>
-                </figure2>
-            <?php endif; ?>
-        </div>
+                    </figure2>
+                <?php endif; ?>
+            </div>
+        <?php else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+        <?php endif; ?>
     </div>                     
 </div>                                 
 
