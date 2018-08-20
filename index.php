29f37cb2d128f5e6bae04416d07ef27dbea83561
boncis <wendigotech@gmail.com>
2018-08-20 15:10:28 +0200

    64

--- index.php
+++ index.php
@@ -4,45 +4,54 @@ get_header(); ?>
 <div class="wrapper" id="index-wrapper"> 
     <div class="container">
         <div class="row grid-content">
-            <figure style="grid-area:2 / 3 / 5 / 4;" class="imghvr-blur">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail( 'normal' );
-                    }
-                 ?>
-                <figcaption>
-                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                    <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                    <?php the_excerpt( ); ?>
-                </figcaption>
-            </figure>
-            <figure style="grid-area:4 / 1 / 6 / 3;" class="imghvr-blur">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail( 'normal' );
-                    }
-                 ?>
-                <figcaption>
-                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                    <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                    <?php the_excerpt( ); ?>
-                </figcaption>
-            </figure>
-            <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail( 'normal' );
-                    }
-                 ?>
-                <figcaption>
-                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                    <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                    <?php the_excerpt( ); ?>
-                </figcaption>
-            </figure>
+            <div class="col-md-4 col-lg-12" style="grid-area:3 / 1 / 4 / 3;">                                  
+
+                <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-lg-12" style="grid-area:1 / 1 / 3 / 3;">                                  
+
+                <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-lg-12" style="grid-area:1 / 3 / 3 / 4;">                                  
+
+                <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>
         </div>
     </div>                     
 </div>                                 
