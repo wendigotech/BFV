35e505078042772d5fe19cea1253f018243368d3
boncis <wendigotech@gmail.com>
2018-08-21 01:41:10 +0200

    87

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper"> 
     <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <div <?php post_class( 'grid-content container' ); ?> id="post-<?php the_ID(); ?>">
+        <div class="grid-content container">
+            <?php while ( have_posts() ) : the_post(); ?>
                 <figure style="grid-area:1 / 1 / 4 / 3;" <?php post_class( 'grid-area:1 / 1 / 4 / 3; imghvr-blur' ); ?>>
                     <?php
                         if ( has_post_thumbnail() ) {
@@ -18,43 +18,43 @@ get_header(); ?>
                         <?php the_excerpt( ); ?>
                     </figcaption>
                 </figure>
-                <figure style="grid-area:1 / 3 / 4 / 4;" <?php post_class( 'grid-area:1 / 3 / 4 / 4; imghvr-blur' ); ?>>
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
-                        <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
-                    </figcaption>
-                </figure>
-                <figure style="grid-area:1 / 3 / 4 / 4;" <?php post_class( 'grid-area:1 / 3 / 4 / 4; imghvr-blur' ); ?>>
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
-                        <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
-                    </figcaption>
-                </figure>
-                <figure style="grid-area:5 / 3 / 8 / 4;" <?php post_class( 'grid-area:5 / 3 / 8 / 4; imghvr-blur' ); ?>>
-                    <img src="https://images.unsplash.com/photo-1518127864129-8d0834d765bc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=687274d645f07306fb95328c405048c5">
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
-                        <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
-                    </figcaption>
-                </figure>
-            </div>
-        <?php endwhile; ?>
+            <?php endwhile; ?>
+            <figure style="grid-area:1 / 3 / 4 / 4;" <?php post_class( 'grid-area:1 / 3 / 4 / 4; imghvr-blur' ); ?>>
+                <?php
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail( 'normal' );
+                    }
+                 ?>
+                <figcaption>
+                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
+                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                    <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
+                    <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
+                </figcaption>
+            </figure>
+            <figure style="grid-area:1 / 3 / 4 / 4;" <?php post_class( 'grid-area:1 / 3 / 4 / 4; imghvr-blur' ); ?>>
+                <?php
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail( 'normal' );
+                    }
+                 ?>
+                <figcaption>
+                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
+                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                    <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
+                    <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
+                </figcaption>
+            </figure>
+            <figure style="grid-area:5 / 3 / 8 / 4;" <?php post_class( 'grid-area:5 / 3 / 8 / 4; imghvr-blur' ); ?>>
+                <img src="https://images.unsplash.com/photo-1518127864129-8d0834d765bc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=687274d645f07306fb95328c405048c5">
+                <figcaption>
+                    <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
+                    <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                    <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
+                    <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
+                </figcaption>
+            </figure>
+        </div>
     <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
     <?php endif; ?> 
