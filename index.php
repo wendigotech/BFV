2873ccd649363c66a1b11f39a443a24acb9fa1f5
boncis <wendigotech@gmail.com>
2018-08-21 01:03:02 +0200

    78

--- index.php
+++ index.php
@@ -14,6 +14,19 @@ get_header(); ?>
     <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
         <div id="post-<?php the_ID(); ?>" <?php post_class( 'wrapper' ); ?>> 
             <div class="grid-content container">
+                <figure style="grid-area:4 / 1 / 6 / 3;" class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
                 <figure style="grid-area:1 / 1 / 4 / 3;" class="imghvr-blur">
                     <?php
                         if ( has_post_thumbnail() ) {
