46599432196c0901226864e9dbf2747833264d6d
boncis <wendigotech@gmail.com>
2018-08-21 02:04:10 +0200

    91

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 get_header(); ?>
 
 <div class="wrapper" id="index-wrapper"> 
-    <?php
-        $casino_args = array()
-    ?>
-    <?php $casino = new WP_Query( $casino_args ); ?>
-    <?php if ( $casino->have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
         <div class="grid-content container">
-            <?php $casino_item_number = 0; ?>
-            <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+            <?php $item_number = 0; ?>
+            <?php while ( have_posts() && $item_number++ < 1 ) : the_post(); ?>
                 <figure style="grid-area:1 / 1 / 4 / 3;" <?php post_class( 'grid-area:1 / 1 / 4 / 3; imghvr-blur' ); ?>>
                     <?php
                         if ( has_post_thumbnail() ) {
@@ -24,10 +20,9 @@ get_header(); ?>
                     </figcaption>
                 </figure>
             <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
-            <?php if ( $casino->have_posts() ) : ?>
-                <?php $casino_item_number = 0; ?>
-                <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+            <?php if ( have_posts() ) : ?>
+                <?php $item_number = 0; ?>
+                <?php while ( have_posts() && $item_number++ < 1 ) : the_post(); ?>
                     <figure style="grid-area:7 / 1 / 5 / 3;" <?php post_class( 'grid-area:1 / 3 / 4 / 4; imghvr-blur' ); ?>>
                         <?php
                             if ( has_post_thumbnail() ) {
@@ -42,7 +37,6 @@ get_header(); ?>
                         </figcaption>
                     </figure>
                 <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
             <?php else : ?>
                 <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
             <?php endif; ?>
@@ -59,9 +53,9 @@ get_header(); ?>
                     <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
                 </figcaption>
             </figure>
-            <?php if ( $casino->have_posts() ) : ?>
-                <?php $casino_item_number = 0; ?>
-                <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+            <?php if ( have_posts() ) : ?>
+                <?php $item_number = 0; ?>
+                <?php while ( have_posts() && $item_number++ < 1 ) : the_post(); ?>
                     <figure style="grid-area:5 / 3 / 8 / 4;" <?php post_class( 'grid-area:5 / 3 / 8 / 4; imghvr-blur' ); ?>>
                         <img src="https://images.unsplash.com/photo-1518127864129-8d0834d765bc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=687274d645f07306fb95328c405048c5">
                         <figcaption>
@@ -72,7 +66,6 @@ get_header(); ?>
                         </figcaption>
                     </figure>
                 <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
             <?php else : ?>
                 <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
             <?php endif; ?>
