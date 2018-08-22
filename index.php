ce2fac77405941b85758e6d71d798652c63bb07b
boncis <wendigotech@gmail.com>
2018-08-23 00:34:00 +0200

    105

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
 get_header(); ?>
 
-<div class="wrapper" id="index-wrapper"> 
-    <?php
-        $casino_args = array(
-            'category_name' => 'casino'
-        )
-    ?>
-    <?php $casino = new WP_Query( $casino_args ); ?>
-    <?php if ( $casino->have_posts() ) : ?>
-        <div class="grid-content container">
-            <div class="row" style="grid-area:1 / 1 / 3 / 3;">
-                <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
-                    <figure class="imghvr-blur">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <figcaption>
-                            <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                            <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                            <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                            <?php the_excerpt( ); ?>
-                        </figcaption>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            </div>
-            <div class="row" style="grid-area:3 / 1 / 5 / 4;">
-                <figure class="imghvr-blur">
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <div id="index-wrapper-<?php the_ID(); ?>" <?php post_class( 'wrapper grid-content' ); ?>> 
+            <div class="container" style="display:grid;grid-template-columns:1fr 1fr 1fr;grid-template-rows:100px;grid-gap:10px;">
+                <figure class="imghvr-blur" style="grid-area:1 / 1 / 2 / 2;">
                     <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail( 'normal' );
@@ -42,9 +18,7 @@ get_header(); ?>
                         <?php the_excerpt( ); ?>
                     </figcaption>
                 </figure>
-            </div>
-            <div class="row" style="grid-area:1 / 3 / 3 / 4;">
-                <figure class="imghvr-blur">
+                <figure class="imghvr-blur" style="grid-area:1 / 3 / 2 / 4;">
                     <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail( 'normal' );
@@ -57,11 +31,24 @@ get_header(); ?>
                         <?php the_excerpt( ); ?>
                     </figcaption>
                 </figure>
-            </div>
+                <figure class="imghvr-blur" style="grid-area:1 / 2 / 2 / 3;">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>                             
         </div>
-    <?php else : ?>
-        <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-    <?php endif; ?> 
-</div>                                 
+    <?php endwhile; ?>
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+<?php endif; ?>                 
 
 <?php get_footer(); ?>
