2c2538f3696e3f0f2ad916ae51d4b83d35ace70b
boncis <wendigotech@gmail.com>
2018-08-23 00:17:47 +0200

    104

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ get_header(); ?>
     <?php $casino = new WP_Query( $casino_args ); ?>
     <?php if ( $casino->have_posts() ) : ?>
         <div class="grid-content container">
-            <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
-                <div class="row" style="grid-area:1 / 1 / 3 / 3;">
+            <div class="row" style="grid-area:1 / 1 / 3 / 3;">
+                <?php while ( $casino->have_posts() ) : $casino->the_post(); ?>
                     <figure class="imghvr-blur">
                         <?php
                             if ( has_post_thumbnail() ) {
@@ -25,9 +25,39 @@ get_header(); ?>
                             <?php the_excerpt( ); ?>
                         </figcaption>
                     </figure>
-                </div>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+            <div class="row" style="grid-area:3 / 1 / 5 / 4;">
+                <figure class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="row" style="grid-area:1 / 3 / 3 / 4;">
+                <figure class="imghvr-blur">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail( 'normal' );
+                        }
+                     ?>
+                    <figcaption>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
+                        <?php the_excerpt( ); ?>
+                    </figcaption>
+                </figure>
+            </div>
         </div>
     <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
