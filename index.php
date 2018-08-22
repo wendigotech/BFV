71db2254e394767e72f33729920b936455598db1
boncis <wendigotech@gmail.com>
2018-08-23 00:37:45 +0200

    106

--- index.php
+++ index.php
@@ -18,30 +18,13 @@ get_header(); ?>
                         <?php the_excerpt( ); ?>
                     </figcaption>
                 </figure>
-                <figure class="imghvr-blur" style="grid-area:1 / 3 / 2 / 4;">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                        <?php the_excerpt( ); ?>
-                    </figcaption>
-                </figure>
                 <figure class="imghvr-blur" style="grid-area:1 / 2 / 2 / 3;">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
+                    <img src="https://images.unsplash.com/photo-1518127864129-8d0834d765bc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=687274d645f07306fb95328c405048c5">
                     <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                        <?php the_excerpt( ); ?>
+                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="#"><?php _e( 'Play', 'st2' ); ?></a>
+                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="#"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                        <h6 class="text-center display-5"><?php _e( 'Heading 6', 'st2' ); ?></h6>
+                        <p class="text-center d-table float-left"><?php _e( 'Paragraph', 'st2' ); ?></p>
                     </figcaption>
                 </figure>
             </div>                             
