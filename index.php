d25f6d0906e52b063545fb78b33dc09fae710904
boncis <wendigotech@gmail.com>
2018-08-23 00:13:43 +0200

    103

--- index.php
+++ index.php
@@ -28,36 +28,6 @@ get_header(); ?>
                 </div>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
-            <div class="row" style="grid-area:3 / 1 / 5 / 4;">
-                <figure class="imghvr-blur">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                        <?php the_excerpt( ); ?>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="row" style="grid-area:1 / 3 / 3 / 4;">
-                <figure class="imghvr-blur">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail( 'normal' );
-                        }
-                     ?>
-                    <figcaption>
-                        <a class="btn active btn-sm d-table btn-warning btn-block" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                        <a class="btn active btn-sm btn-danger d-table" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
-                        <h6 class="text-center display-5"><?php the_title(); ?></h6>
-                        <?php the_excerpt( ); ?>
-                    </figcaption>
-                </figure>
-            </div>
         </div>
     <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
