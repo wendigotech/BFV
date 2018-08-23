a83bc84ffbb9784f75ead922284a9919b6ebf6aa
boncis <wendigotech@gmail.com>
2018-08-24 00:30:43 +0200

    Main content Tab 2 WP Loop Edit

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ get_header(); ?>
 
 <div class="wrapper grid-content" id="index-wrapper"> 
     <ul class="nav nav-pills" role="tablist" style="grid-area:1 / 2 / 2 / 8;"> 
-        <li class="nav-item"> 
-            <a class="nav-link active" href="#tab1" data-toggle="tab" role="tab" aria-controls="tab1" aria-expanded="true"><?php _e( 'Home', 'st2' ); ?></a> 
+        <li class="nav-item align-self-center"> 
+            <a class="nav-link active" href="#tab1" data-toggle="tab" role="tab" aria-controls="tab1" aria-expanded="true"><?php _e( 'Casino', 'st2' ); ?></a> 
         </li>                         
 
-        <li class="nav-item"> 
-            <a class="nav-link" href="#tab2" data-toggle="tab" role="tab" aria-controls="tab2" aria-expanded="true"><?php _e( 'Profile', 'st2' ); ?></a> 
+        <li class="nav-item align-self-center"> 
+            <a class="nav-link" href="#tab2" data-toggle="tab" role="tab" aria-controls="tab2" aria-expanded="true"><?php _e( 'Slots', 'st2' ); ?></a> 
         </li>                         
 
-        <li class="nav-item"> 
-            <a class="nav-link" href="#tab3" data-toggle="tab" role="tab" aria-controls="tab3" aria-expanded="true"><?php _e( 'Messages', 'st2' ); ?></a> 
+        <li class="nav-item align-self-center"> 
+            <a class="nav-link" href="#tab3" data-toggle="tab" role="tab" aria-controls="tab3" aria-expanded="true"><?php _e( 'Bingo', 'st2' ); ?></a> 
         </li>                                                  
     </ul>
     <div class="tab-content" style="grid-area:1 / 1 / 2 / 9;">
@@ -158,17 +158,17 @@ get_header(); ?>
         <div class="tab-pane" role="tabpanel" aria-labelledby="tab2-tab" id="tab2">
             <div class="container grid-content">
                 <?php
-                    $casino_args = array(
-                        'category_name' => 'casino',
+                    $Slots_args = array(
+                        'category_name' => 'slots',
                         'nopaging' => true,
                         'order' => 'ASC',
                         'orderby' => 'date'
                     )
                 ?>
-                <?php $casino = new WP_Query( $casino_args ); ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php $Slots = new WP_Query( $Slots_args ); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:2 / 2 / 5 / 6;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -187,9 +187,9 @@ get_header(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                 <?php endif; ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:5 / 2 / 9 / 4;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -208,9 +208,9 @@ get_header(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                 <?php endif; ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:7 / 4 / 9 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -229,9 +229,9 @@ get_header(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                 <?php endif; ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:2 / 6 / 4 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -250,9 +250,9 @@ get_header(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                 <?php endif; ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:5 / 4 / 7 / 6;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -271,9 +271,9 @@ get_header(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                 <?php endif; ?>
-                <?php if ( $casino->have_posts() ) : ?>
-                    <?php $casino_item_number = 0; ?>
-                    <?php while ( $casino->have_posts() && $casino_item_number++ < 1 ) : $casino->the_post(); ?>
+                <?php if ( $Slots->have_posts() ) : ?>
+                    <?php $Slots_item_number = 0; ?>
+                    <?php while ( $Slots->have_posts() && $Slots_item_number++ < 1 ) : $Slots->the_post(); ?>
                         <figure style="grid-area:4 / 6 / 7 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
                             <?php
                                 if ( has_post_thumbnail() ) {
