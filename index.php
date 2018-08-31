c0df2c7650af8af05d068473b7d256ef68d777df
boncis <wendigotech@gmail.com>
2018-09-01 00:27:38 +0200

    add hint small class

--- index.php
+++ index.php
@@ -450,7 +450,7 @@ get_header(); ?>
                             <a href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>">
                                 <figcaption>
                                     <a class="btn active btn-sm btn-warning hvr-sweep-to-top" role="button" aria-pressed="true" data-html="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                                    <a class="btn active btn-sm btn-danger hvr-sweep-to-left hint--bottom" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                                    <a class="btn active btn-sm btn-danger hvr-sweep-to-left hint--bottom hint--small" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
                                     <h6 class="text-center display-5 "><?php the_title(); ?></h6>
                                     <?php the_excerpt( ); ?>
                                 </figcaption>
@@ -514,7 +514,7 @@ get_header(); ?>
                              ?>
                             <figcaption>
                                 <a class="btn active btn-sm btn-warning hvr-sweep-to-top" role="button" aria-pressed="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a>
-                                <a class="btn active btn-sm btn-danger hvr-sweep-to-left hint--bottom" role="button" aria-pressed="true" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
+                                <a class="btn active btn-sm btn-danger hvr-sweep-to-left hint--bottom hint--small" role="button" aria-pressed="true" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a>
                                 <h6 class="text-center display-5"><?php the_title(); ?></h6>
                                 <?php the_excerpt( ); ?>
                             </figcaption>
