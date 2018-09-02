b634ad9febd45abc4b96801825cb433409825215
boncis <wendigotech@gmail.com>
2018-09-03 01:43:34 +0200

    faq page

---
+++ page-faq.php
@@ -0,0 +1,31 @@
+<?php
+get_header(); ?>
+
+<div id="page-wrapper" class="wrapper"> 
+    <div class="container" id="content" tabindex="-1"> 
+        <div class="row">                              
+            <div class="content-area col-md-12" id="primary"> 
+                <main class="site-main" id="main"> 
+                    <div> 
+                        <?php if ( have_posts() ) : ?>
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
+                                    <header class="entry-header"> 
+                                        <h1 class="text-center"><?php the_title(); ?></h1>                                                          
+                                    </header>                                                     
+                                    <div class="entry-content"> 
+                                        <?php the_content(); ?>                                                          
+                                    </div>                                                                                                          
+                                </article>
+                            <?php endwhile; ?>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                        <?php endif; ?>                                          
+                    </div>                                     
+                </main>                                 
+            </div>                                                          
+        </div>                         
+    </div>                     
+</div>                                 
+
+<?php get_footer(); ?>
