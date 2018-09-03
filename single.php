947fab967f3df7a401573f5e9ff39a5f8ff678ce
boncis <wendigotech@gmail.com>
2018-09-03 02:34:47 +0200

    174

--- single.php
+++ single.php
@@ -4,7 +4,7 @@ get_header(); ?>
 <div class="wrapper" id="single-wrapper"> 
     <div class="container" id="content" tabindex="-1"> 
         <div class="row">                              
-            <div class="content-area col-md-8 content-area" id="primary"> 
+            <div class="content-area col-md-12" id="primary"> 
                 <main class="site-main" id="main"> 
                     <div> 
                         <?php if ( have_posts() ) : ?>
@@ -31,14 +31,7 @@ get_header(); ?>
                         </nav>                                                                                  
                     </div>                                     
                 </main>                                 
-            </div>                             
-            <?php if ( get_theme_mod( 'show_right_sidebar' ) ) : ?>
-                <div class="col-md-4 widget-area" role="complementary" id="right-sidebar"> 
-                    <?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
-                        <?php dynamic_sidebar( 'right-sidebar' ); ?>
-                    <?php endif; ?>                                                                           
-                </div>
-            <?php endif; ?> 
+            </div>                                                          
         </div>                         
     </div>                     
 </div>                                 
