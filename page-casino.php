fc3373c2d02c589a46620cd9352d7118bfc2a1ef
boncis <wendigotech@gmail.com>
2018-09-05 23:32:35 +0200

    casino posts descending

--- page-casino.php
+++ page-casino.php
@@ -8,7 +8,7 @@ get_header(); ?>
                 $casino_args = array(
                     'category_name' => 'casino',
                     'nopaging' => true,
-                    'order' => 'ASC',
+                    'order' => 'DESC',
                     'orderby' => 'date'
                 )
             ?>
