011c41adf70afc243ecfb6851a8d72409f119be1
boncis <wendigotech@gmail.com>
2018-09-08 00:08:18 +0200

    tooltip size - medium

--- index.php
+++ index.php
@@ -639,7 +639,7 @@ get_header(); ?>
                                 <figcaption>
                                     <div class="row pg-empty-placeholder">
                                         <div class="col-md-6">
-                                            <a class="btn active hvr-sweep-to-left hint--bottom hint--small h-auto w-auto shadow-sm text-light btn-sm border border-light" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a> 
+                                            <a class="btn active hvr-sweep-to-left hint--bottom h-auto w-auto shadow-sm text-light btn-sm border border-light hint--medium" aria-label="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a> 
                                         </div>
                                         <div class="col-md-6">
                                             <a class="btn active hvr-sweep-to-top btn-light shadow-sm bg-light float-right text-uppercase font-weight-bold text-success btn-sm" role="button" aria-pressed="true" data-html="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a> 
