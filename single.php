c578f2d402fcb3ed6f5900a2581d5b0e99c0745b
boncis <wendigotech@gmail.com>
2018-09-11 00:11:21 +0200

    214

--- single.php
+++ single.php
@@ -22,34 +22,24 @@ get_header(); ?>
                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                         <?php endif; ?> 
                         <div class="container">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <figure class="imghvr-blur">
-                                        <?php
-                                            if ( has_post_thumbnail() ) {
-                                                the_post_thumbnail( 'normal' );
-                                            }
-                                         ?>
-                                        <a href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>">
-                                            <?php _e( 'Link', 'st2' ); ?>
-                                            <figcaption>
-                                                <div class="row pg-empty-placeholder">
-                                                    <div class="col-md-6">
-                                                        <a class="btn active hvr-sweep-to-left h-auto w-auto shadow-sm text-light btn-sm border border-light" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>" title="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a> 
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <a class="btn active hvr-sweep-to-top btn-light shadow-sm bg-light float-right text-uppercase font-weight-bold text-success btn-sm" role="button" aria-pressed="true" data-html="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a> 
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12"> 
-                                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                                    <?php the_excerpt( ); ?>
-                                                </div>
-                                            </figcaption>
-                                        </a>
-                                    </figure>                                                     
-                                </div>
-                            </div>
+                            <?php if ( ($tags) { echo 'Related Posts' ) : ?>
+                                <?php
+                                    $related_args = array(
+                                        'tag__in' => 'term_id',
+                                        'nopaging' => true,
+                                        'order' => 'ASC',
+                                        'orderby' => 'date'
+                                    )
+                                ?>
+                                <?php $related = new WP_Query( $related_args ); ?>
+                                <?php if ( $related->have_posts() ) : ?>
+                                    <div <?php post_class( 'row' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php echo $tags = wp_get_post_tags($post->ID); ?>
+                                    </div>
+                                <?php else : ?>
+                                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                                <?php endif; ?>
+                            <?php endif; ?>
                         </div>                                         
                     </div>                                     
                 </main>                                 
