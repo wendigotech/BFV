c72532ab31c3666f48939a64e71ec9eb37e41a3a
boncis <wendigotech@gmail.com>
2018-09-13 00:50:45 +0200

    264

--- single.php
+++ single.php
@@ -14,7 +14,8 @@ get_header(); ?>
                         ?>
                         <?php $tags = new WP_Query( $tags_args ); ?>
                         <?php if ( $tags->have_posts() ) : ?>
-                            <?php while ( $tags->have_posts() ) : $tags->the_post(); ?>
+                            <?php $tags_item_number = 0; ?>
+                            <?php while ( $tags->have_posts() && $tags_item_number++ < 1 ) : $tags->the_post(); ?>
                                 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
                                     <header class="entry-header"> 
                                         <h1 class="text-center text-light"><?php the_title(); ?></h1> 
@@ -31,30 +32,38 @@ get_header(); ?>
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <figure class="imghvr-blur">
-                                        <?php
-                                            if ( has_post_thumbnail() ) {
-                                                the_post_thumbnail( 'normal' );
-                                            }
-                                         ?>
-                                        <a href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>">
-                                            <?php _e( 'Link', 'st2' ); ?>
-                                            <figcaption>
-                                                <div class="row pg-empty-placeholder">
-                                                    <div class="col-md-6">
-                                                        <a class="btn active hvr-sweep-to-left h-auto w-auto shadow-sm text-light btn-sm border border-light" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>" data-toggle="tooltip" data-placement="bottom" data-trigger="hover" title="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a> 
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <a class="btn active hvr-sweep-to-top btn-light shadow-sm bg-light float-right text-uppercase font-weight-bold text-success btn-sm" role="button" aria-pressed="true" data-html="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a> 
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12"> 
-                                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                                    <?php the_excerpt( ); ?>
-                                                </div>
-                                            </figcaption>
-                                        </a>
-                                    </figure>                                                     
+                                    <?php if ( $tags->have_posts() ) : ?>
+                                        <?php $tags_item_number = 0; ?>
+                                        <?php while ( $tags->have_posts() && $tags_item_number++ < 1 ) : $tags->the_post(); ?>
+                                            <figure class="imghvr-blur">
+                                                <?php
+                                                    if ( has_post_thumbnail() ) {
+                                                        the_post_thumbnail( 'normal' );
+                                                    }
+                                                 ?>
+                                                <a href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>">
+                                                    <?php _e( 'Link', 'st2' ); ?>
+                                                    <figcaption>
+                                                        <div class="row pg-empty-placeholder">
+                                                            <div class="col-md-6">
+                                                                <a class="btn active hvr-sweep-to-left h-auto w-auto shadow-sm text-light btn-sm border border-light" role="button" aria-pressed="true" data-html="false" href="<?php echo get_post_meta( get_the_ID(), 'T&C´s link', true ); ?>" data-toggle="tooltip" data-placement="bottom" data-trigger="hover" title="<?php echo get_post_meta( get_the_ID(), 'Key', true ); ?>"><?php _e( 'T&C´s', 'st2' ); ?></a> 
+                                                            </div>
+                                                            <div class="col-md-6">
+                                                                <a class="btn active hvr-sweep-to-top btn-light shadow-sm bg-light float-right text-uppercase font-weight-bold text-success btn-sm" role="button" aria-pressed="true" data-html="true" href="<?php echo get_post_meta( get_the_ID(), 'Link', true ); ?>"><?php _e( 'Play', 'st2' ); ?></a> 
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-12"> 
+                                                            <h6 class="text-center display-5 "><?php the_title(); ?></h6>
+                                                            <?php the_excerpt( ); ?>
+                                                        </div>
+                                                    </figcaption>
+                                                </a>
+                                            </figure>
+                                        <?php endwhile; ?>
+                                        <?php wp_reset_postdata(); ?>
+                                    <?php else : ?>
+                                        <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                                    <?php endif; ?> 
                                 </div>
                                 <div class="col-md-4"> 
                                     <figure class="imghvr-blur">
