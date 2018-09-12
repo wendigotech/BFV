7fc12d4ec657913e06048e08c1f744725f7ca2cf
boncis <wendigotech@gmail.com>
2018-09-13 00:22:18 +0200

    258

--- single.php
+++ single.php
@@ -8,17 +8,17 @@ get_header(); ?>
                 <main class="site-main" id="main"> 
                     <div> 
                         <?php if ( have_posts() ) : ?>
-                            <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
-                                <header class="entry-header"> 
-                                    <h1 class="text-center text-light"><?php the_title(); ?></h1> 
-                                </header>                                                 
-                                <div class="entry-content"> 
-                                    <?php the_content(); ?> 
-                                </div>                                                 
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <?php while ( have_posts() ) : the_post(); ?>
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
+                                    <header class="entry-header"> 
+                                        <h1 class="text-center text-light"><?php the_title(); ?></h1> 
+                                    </header>                                                     
+                                    <div class="entry-content"> 
+                                        <?php the_content(); ?> 
+                                    </div>                                                     
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-md-4">
                                                 <figure class="imghvr-blur">
                                                     <?php
                                                         if ( has_post_thumbnail() ) {
@@ -42,30 +42,30 @@ get_header(); ?>
                                                             </div>
                                                         </figcaption>
                                                     </a>
+                                                </figure>                                                                 
+                                            </div>
+                                            <div class="col-md-4"> 
+                                                <figure class="imghvr-blur">
+                                                    <?php
+                                                        if ( has_post_thumbnail() ) {
+                                                            the_post_thumbnail( 'normal' );
+                                                        }
+                                                     ?>
                                                 </figure>
-                                            <?php endwhile; ?> 
-                                        </div>
-                                        <div class="col-md-4"> 
-                                            <figure class="imghvr-blur">
-                                                <?php
-                                                    if ( has_post_thumbnail() ) {
-                                                        the_post_thumbnail( 'normal' );
-                                                    }
-                                                 ?>
-                                            </figure>
-                                        </div>
-                                        <div class="col-md-4"> 
-                                            <figure class="imghvr-blur">
-                                                <?php
-                                                    if ( has_post_thumbnail() ) {
-                                                        the_post_thumbnail( 'normal' );
-                                                    }
-                                                 ?>
-                                            </figure>
+                                            </div>
+                                            <div class="col-md-4"> 
+                                                <figure class="imghvr-blur">
+                                                    <?php
+                                                        if ( has_post_thumbnail() ) {
+                                                            the_post_thumbnail( 'normal' );
+                                                        }
+                                                     ?>
+                                                </figure>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </article>
+                                </article>
+                            <?php endwhile; ?>
                         <?php else : ?>
                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                         <?php endif; ?>                                          
