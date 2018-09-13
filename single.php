af505d886026e650ba4973a3491bc902e9028e76
boncis <wendigotech@gmail.com>
2018-09-13 22:24:03 +0200

    279

--- single.php
+++ single.php
@@ -22,11 +22,9 @@ get_header(); ?>
                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                         <?php endif; ?>
                         <?php if ( have_posts() ) : ?>
-                            <div class="container">
-                                    <div class="row">
-                                    <?php $orig_post = $post;   global $post;   $tags = wp_get_post_tags($post->ID);       if ($tags) {   $tag_ids = array();   foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;   $args=array(   'tag__in' => $tag_ids,   'post__not_in' => array($post->ID),   'posts_per_page'=>4, // Number of related posts to display.   'caller_get_posts'=>1   );       $my_query = new wp_query( $args );     while( $my_query->have_posts() ) {   $my_query->the_post(); ?>
-                                </div>
-                                <?php $post = $orig_post;   wp_reset_query(); ?>
+                                <div class="container">
+                                <?php $orig_post = $post;   global $post;   $tags = wp_get_post_tags($post->ID);       if ($tags) {   $tag_ids = array();   foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;   $args=array(   'tag__in' => $tag_ids,   'post__not_in' => array($post->ID),   'posts_per_page'=>4, // Number of related posts to display.   'caller_get_posts'=>1   );       $my_query = new wp_query( $args );     while( $my_query->have_posts() ) {   $my_query->the_post(); ?>
+                                    <?php $post = $orig_post;   wp_reset_query(); ?>
                             </div>
                         <?php else : ?>
                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
