ff2daed0814d4d8ca4da262bba5a8bc5d01018d8
boncis <wendigotech@gmail.com>
2018-09-13 23:06:21 +0200

    288

--- single.php
+++ single.php
@@ -21,9 +21,9 @@ get_header(); ?>
                                         <?php else : ?>
                                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                                         <?php endif; ?>
-                                        <div class="container">
+                                            <div class="container">
+                                            <?php $orig_post = $post;   global $post;   $tags = wp_get_post_tags($post->ID);       if ($tags) {   $tag_ids = array();   foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;   $args=array(   'tag__in' => $tag_ids,   'post__not_in' => array($post->ID),   'posts_per_page'=>4, // Number of related posts to display.   'caller_get_posts'=>1   );       $my_query = new wp_query( $args );     while( $my_query->have_posts() ) {   $my_query->the_post(); ?>
                                                 <div class="row">
-                                                <?php $orig_post = $post;   global $post;   $tags = wp_get_post_tags($post->ID);       if ($tags) {   $tag_ids = array();   foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;   $args=array(   'tag__in' => $tag_ids,   'post__not_in' => array($post->ID),   'posts_per_page'=>4, // Number of related posts to display.   'caller_get_posts'=>1   );       $my_query = new wp_query( $args );     while( $my_query->have_posts() ) {   $my_query->the_post(); ?>
                                                     <div class="col-md-4">
                                                         <figure class="imghvr-blur">
                                                             <?php
@@ -54,8 +54,8 @@ get_header(); ?>
 </div>
                                                     <div class="col-md-4"> 
 </div>
-                                            </div>
-                                            <?php $post = $orig_post;   wp_reset_query(); ?>
+                                                </div>
+                                                <?php $post = $orig_post;   wp_reset_query(); ?>
                                         </div>                                         
                                     </div>                                     
                                 </main>                                 
