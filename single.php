cf3bdbc1baeeb753b22ca121c6a61f2128946a74
boncis <wendigotech@gmail.com>
2018-09-15 00:15:17 +0200

    295

--- single.php
+++ single.php
@@ -22,15 +22,14 @@ get_header(); ?>
                                             <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
                                         <?php endif; ?>
                                         <div class="container">
-                                            <div class="row pg-empty-placeholder"></div>
                                             <?php
-                                                $related_args = array()
+                                                $my_query_args = array()
                                             ?>
-                                            <?php $related = new WP_Query( $related_args ); ?>
-                                            <?php if ( $related->have_posts() ) : ?>
-                                                <div <?php post_class( 'row' ); ?> id="post-<?php the_ID(); ?>">
+                                            <?php $my_query = new WP_Query( $my_query_args ); ?>
+                                            <?php if ( $my_query->have_posts() ) : ?>
+                                                <div class="row">
                                                     <div class="col-md-4">
-                                                        <?php while ( $related->have_posts() ) : $related->the_post(); ?>
+                                                        <?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
                                                             <figure class="imghvr-blur">
                                                                 <?php
                                                                     if ( has_post_thumbnail() ) {
@@ -59,7 +58,27 @@ get_header(); ?>
                                                         <?php wp_reset_postdata(); ?> 
                                                     </div>
                                                     <div class="col-md-4"> 
-</div>
+                                                        <figure class="imghvr-blur">
+                                                            <img src="https://images.unsplash.com/photo-1518127864129-8d0834d765bc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=687274d645f07306fb95328c405048c5">
+                                                            <a href="">
+                                                                <?php _e( 'Link', 'st2' ); ?>
+                                                                <figcaption>
+                                                                    <div class="row pg-empty-placeholder">
+                                                                        <div class="col-md-6">
+                                                                            <a class="btn active hvr-sweep-to-left h-auto w-auto shadow-sm text-light btn-sm border border-light" role="button" aria-pressed="true" data-html="false" href="#" data-toggle="tooltip" data-placement="bottom" data-trigger="hover"><?php _e( 'T&C´s', 'st2' ); ?></a> 
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <a class="btn active hvr-sweep-to-top btn-light shadow-sm bg-light float-right text-uppercase font-weight-bold text-success btn-sm" role="button" aria-pressed="true" data-html="true" href="#"><?php _e( 'Play', 'st2' ); ?></a> 
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-12"> 
+                                                                        <h6 class="text-center display-5 "><?php _e( 'Heading 6', 'st2' ); ?></h6>
+                                                                        <p class="text-center"><?php _e( 'Paragraph', 'st2' ); ?></p>
+                                                                    </div>
+                                                                </figcaption>
+                                                            </a>
+                                                        </figure>
+                                                    </div>
                                                     <div class="col-md-4"> 
 </div>
                                                 </div>
