764bef84d186e7fec4804ea99a88cd1421641a66
boncis <wendigotech@gmail.com>
2018-09-18 00:04:26 +0200

    332

--- page-faq.php
+++ page-faq.php
@@ -7,24 +7,20 @@ get_header(); ?>
             <div class="content-area col-md-12" id="primary"> 
                 <main class="site-main" id="main"> 
                     <div> 
-                        <article class=""> 
-                            <header class="entry-header"> 
-                                <h1 class="text-center text-light"><?php the_title(); ?></h1> 
-                            </header>                                             
-                            <?php if ( have_posts() ) : ?>
-                                <?php while ( have_posts() ) : the_post(); ?>
-                                    <div <?php post_class( 'entry-content' ); ?> id="post-<?php the_ID(); ?>"> 
-                                        <div id="exampleAccordion" data-children=".item">
-                                            <div class="item">
-                                                <?php the_content(); ?>
-                                            </div>
-                                        </div>                                                         
-                                    </div>
-                                <?php endwhile; ?>
-                            <?php else : ?>
-                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-                            <?php endif; ?> 
-                        </article>                                         
+                        <?php if ( have_posts() ) : ?>
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
+                                    <header class="entry-header"> 
+                                        <h1 class="text-center text-light"><?php the_title(); ?></h1> 
+                                    </header>                                                     
+                                    <div class="entry-content">
+                                        <?php the_content(); ?>
+                                    </div>                                                     
+                                </article>
+                            <?php endwhile; ?>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                        <?php endif; ?> 
                     </div>                                     
                 </main>                                 
             </div>                             
