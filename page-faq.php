703264406b0155d2f6ba15143c4e886765bbb05e
boncis <wendigotech@gmail.com>
2018-09-17 23:46:41 +0200

    330

--- page-faq.php
+++ page-faq.php
@@ -7,24 +7,27 @@ get_header(); ?>
             <div class="content-area col-md-12" id="primary"> 
                 <main class="site-main" id="main"> 
                     <div> 
-                        <?php if ( have_posts() ) : ?>
-                            <?php while ( have_posts() ) : the_post(); ?>
-                                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
-                                    <header class="entry-header"> 
-                                        <h1 class="text-center text-light"><?php the_title(); ?></h1> 
-                                    </header>                                                     
-                                    <div class="entry-content"> 
+                        <article class=""> 
+                            <header class="entry-header"> 
+                                <h1 class="text-center text-light"><?php the_title(); ?></h1> 
+                            </header>                                             
+                            <?php if ( have_posts() ) : ?>
+                                <?php while ( have_posts() ) : the_post(); ?>
+                                    <div <?php post_class( 'entry-content' ); ?> id="post-<?php the_ID(); ?>"> 
                                         <div id="exampleAccordion" data-children=".item">
                                             <div class="item">
-                                                <?php the_content(); ?>
+                                                <a data-toggle="collapse" data-parent="#exampleAccordion" href="#exampleAccordion2" aria-expanded="false" aria-controls="exampleAccordion2"><h3><?php the_content(); ?></h3></a>
+                                                <div id="exampleAccordion2" class="collapse" role="tabpanel">
+                                                    <p class="mb-3"><?php the_content(); ?></p>
+                                                </div>
                                             </div>
                                         </div>                                                         
-                                    </div>                                                     
-                                </article>
-                            <?php endwhile; ?>
-                        <?php else : ?>
-                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-                        <?php endif; ?> 
+                                    </div>
+                                <?php endwhile; ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?> 
+                        </article>                                         
                     </div>                                     
                 </main>                                 
             </div>                             
