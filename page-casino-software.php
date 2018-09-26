047aaad909b17e1bab2fbc335b71f7adeb923a53
boncis <wendigotech@gmail.com>
2018-09-26 22:33:04 +0200

    remove figcaption text

--- page-casino-software.php
+++ page-casino-software.php
@@ -1,140 +1,120 @@
 <?php
 get_header(); ?>
 
-<div id="page-wrapper" class="wrapper"> 
-    <div class="container" id="content" tabindex="-1"> 
-        <div class="container grid-4">
-            <?php
-                $Software_args = array(
-                    'category_name' => 'software',
-                    'nopaging' => true,
-                    'order' => 'DESC',
-                    'orderby' => 'date'
-                )
-            ?>
-            <?php $Software = new WP_Query( $Software_args ); ?>
-            <?php if ( $Software->have_posts() ) : ?>
-                <?php $Software_item_number = 0; ?>
-                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
-                    <figure style="grid-area:2 / 2 / 4 / 5;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <a href="<?php echo esc_url( get_permalink() ); ?>">
-                            <?php _e( 'Link', 'st2' ); ?>
-                            <figcaption>
-                                <div class="col-md-12"> 
-                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                    <?php the_content(); ?>
-                                </div>
-                            </figcaption>
-                        </a>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-            <?php if ( $Software->have_posts() ) : ?>
-                <?php $Software_item_number = 0; ?>
-                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
-                    <figure style="grid-area:2 / 5 / 4 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <a href="<?php echo esc_url( get_permalink() ); ?>">
-                            <?php _e( 'Link', 'st2' ); ?>
-                            <figcaption>
-                                <div class="col-md-12"> 
-                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                    <?php the_content(); ?>
-                                </div>
-                            </figcaption>
-                        </a>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-            <?php if ( $Software->have_posts() ) : ?>
-                <?php $Software_item_number = 0; ?>
-                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
-                    <figure style="grid-area:4 / 6 / 7 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <a href="<?php echo esc_url( get_permalink() ); ?>">
-                            <?php _e( 'Link', 'st2' ); ?>
-                            <figcaption>
-                                <div class="col-md-12"> 
-                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                    <?php the_content(); ?>
-                                </div>
-                            </figcaption>
-                        </a>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-            <?php if ( $Software->have_posts() ) : ?>
-                <?php $Software_item_number = 0; ?>
-                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
-                    <figure style="grid-area:4 / 2 / 7 / 5;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <a href="<?php echo esc_url( get_permalink() ); ?>">
-                            <?php _e( 'Link', 'st2' ); ?>
-                            <figcaption>
-                                <div class="col-md-12"> 
-                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                    <?php the_content(); ?>
-                                </div>
-                            </figcaption>
-                        </a>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-            <?php if ( $Software->have_posts() ) : ?>
-                <?php $Software_item_number = 0; ?>
-                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
-                    <figure style="grid-area:4 / 5 / 7 / 6;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal' );
-                            }
-                         ?>
-                        <a href="<?php echo esc_url( get_permalink() ); ?>">
-                            <?php _e( 'Link', 'st2' ); ?>
-                            <figcaption>
-                                <div class="col-md-12"> 
-                                    <h6 class="text-center display-5 "><?php the_title(); ?></h6>
-                                    <?php the_content(); ?>
-                                </div>
-                            </figcaption>
-                        </a>
-                    </figure>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
-            <?php endif; ?>
-        </div>
-    </div>                     
-</div>                                 
+                <div id="page-wrapper" class="wrapper"> 
+                    <div class="container" id="content" tabindex="-1"> 
+                        <div class="container grid-4">
+                            <?php
+                                $Software_args = array(
+                                    'category_name' => 'software',
+                                    'nopaging' => true,
+                                    'order' => 'DESC',
+                                    'orderby' => 'date'
+                                )
+                            ?>
+                            <?php $Software = new WP_Query( $Software_args ); ?>
+                            <?php if ( $Software->have_posts() ) : ?>
+                                <?php $Software_item_number = 0; ?>
+                                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
+                                    <figure style="grid-area:2 / 2 / 4 / 5;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( 'normal' );
+                                            }
+                                         ?>
+                                        <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                            <?php _e( 'Link', 'st2' ); ?>
+                                            <figcaption>
+</figcaption>
+                                        </a>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?>
+                            <?php if ( $Software->have_posts() ) : ?>
+                                <?php $Software_item_number = 0; ?>
+                                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
+                                    <figure style="grid-area:2 / 5 / 4 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( 'normal' );
+                                            }
+                                         ?>
+                                        <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                            <?php _e( 'Link', 'st2' ); ?>
+                                            <figcaption>
+</figcaption>
+                                        </a>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?>
+                            <?php if ( $Software->have_posts() ) : ?>
+                                <?php $Software_item_number = 0; ?>
+                                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
+                                    <figure style="grid-area:4 / 6 / 7 / 8;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( 'normal' );
+                                            }
+                                         ?>
+                                        <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                            <?php _e( 'Link', 'st2' ); ?>
+                                            <figcaption>
+</figcaption>
+                                        </a>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?>
+                            <?php if ( $Software->have_posts() ) : ?>
+                                <?php $Software_item_number = 0; ?>
+                                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
+                                    <figure style="grid-area:4 / 2 / 7 / 5;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( 'normal' );
+                                            }
+                                         ?>
+                                        <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                            <?php _e( 'Link', 'st2' ); ?>
+                                            <figcaption>
+</figcaption>
+                                        </a>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?>
+                            <?php if ( $Software->have_posts() ) : ?>
+                                <?php $Software_item_number = 0; ?>
+                                <?php while ( $Software->have_posts() && $Software_item_number++ < 1 ) : $Software->the_post(); ?>
+                                    <figure style="grid-area:4 / 5 / 7 / 6;" <?php post_class( 'imghvr-blur' ); ?> id="post-<?php the_ID(); ?>">
+                                        <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( 'normal' );
+                                            }
+                                         ?>
+                                        <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                            <?php _e( 'Link', 'st2' ); ?>
+                                            <figcaption>
+</figcaption>
+                                        </a>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'st2' ); ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </div>                     
+                </div>                                 
 
 <?php get_footer(); ?>
